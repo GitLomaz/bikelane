84d8c0dbe2e037b6a7c7268fb8f3a3e8184cae0f
Ian Lomas <you@example.com>
2026-01-15 14:55:10 -0500

    AUTO-DEPLOYMENT

---
+++ zen/js/classes/player.js
@@ -0,0 +1,113 @@
+class Player extends Phaser.GameObjects.Container {
+  constructor() {
+    super(scene, 300, 600
+    );
+    // this.setScale(LANE_POSITIONS[2].scale, LANE_POSITIONS[2].scale)
+    this.setName("player");
+    scene.add.existing(this);
+
+    this.sprite = scene.add.sprite(0, 0, "bike")
+    this.sprite.setOrigin(1)
+
+    scene.anims.create({
+        key: 'bike',
+        frames: 'bike',
+        frameRate: 15,
+        repeat: -1
+    });
+
+    this.sprite.play({ key: 'bike' });
+    this.add(this.sprite)
+    
+    // Lane system
+    this.lane = 2; // 1, 2, or 3
+    this.switchingLane = false
+    
+    // Movement system
+    this.velocity = { x: 0, y: 0 };
+    this.speed = 5; // Pixels per frame
+    this.minX = 0;
+    this.maxX = GAME_WIDTH - 50;
+    
+    this.setupInput();
+    scene.events.on("update", () => this.update());
+
+    this.playerBlip = new Blip(GAME_WIDTH - (this.lane * 30) - 10, 207)
+    this.playerBlip.setTintFill(0x00FF00)
+  }
+
+  setupInput() {
+    const keys = scene.input.keyboard.createCursorKeys();
+    const wasdKeys = scene.input.keyboard.addKeys('W,A,S,D');
+    this.keys = { ...keys, ...wasdKeys };
+  }
+
+  update() {
+    return
+    // Lane switching (up/down or w/s)
+    if (this.keys.up.isDown || this.keys.W.isDown) {
+      if (this.lane > 1) {
+        this.switchLane(this.lane - 1);
+      }
+    }
+    
+    if (this.keys.down.isDown || this.keys.S.isDown) {
+      if (this.lane < 3) {
+        this.switchLane(this.lane + 1);
+      }
+    }
+    
+    // Left/Right movement (smooth)
+    if (this.keys.left.isDown || this.keys.A.isDown) {
+      setSpeedMod(.3)
+    } else if (this.keys.right.isDown || this.keys.D.isDown) {
+      setSpeedMod(1.7)
+    } else {
+      setSpeedMod(1)
+    }
+    
+    // // Apply horizontal movement
+    // this.x += this.velocity.x;
+    
+    // Clamp position to screen bounds
+    // this.x = Phaser.Math.Clamp(this.x, this.minX, this.maxX);
+  }
+
+  switchLane(newLane) {
+    if (this.switchingLane) {
+      return
+    }
+    this.switchingLane = true
+    const targetPosition = LANE_POSITIONS[newLane];
+    
+    scene.tweens.add({
+      targets: this,
+      y: targetPosition.y,
+      scaleX: targetPosition.scale,
+      scaleY: targetPosition.scale,
+      duration: 300,
+      ease: "Quad.easeInOut",
+      onUpdate: (tween) => {
+      if (tween.progress >= 0.5 && !tween.hasReachedMidpoint) {
+        tween.hasReachedMidpoint = true;
+        this.onLaneSwitchMidpoint(newLane);
+      }
+      },
+      onComplete: () => {
+      this.switchingLane = false;
+      }
+    });
+    
+    scene.tweens.add({
+      targets: this.playerBlip,
+      x: GAME_WIDTH - (newLane * 30) - 10,
+      duration: 300,
+      ease: "Quad.easeInOut",
+    });
+  }
+
+  onLaneSwitchMidpoint(newLane) {
+    this.lane = newLane
+  }
+}
+
---
+++ zen/js/scenes/gameScene.js
@@ -0,0 +1,66 @@
+let gameScene = new Phaser.Class({
+  Extends: Phaser.Scene,
+  initialize: function gameScene() {
+    Phaser.Scene.call(this, {
+      key: "gameScene",
+    });
+  },
+
+  preload: function () {
+    this.load.image("radar", "images/radar.png");
+    this.load.image("player", "images/player.png");
+    this.load.image("blip", "images/blip.png");
+    
+    
+    this.load.image("bg", "images/bg.png");
+    this.load.image("bg2", "images/bg2.png");
+    this.load.image("bg3", "images/bg3.png");
+    this.load.image("bg4", "images/bg4.png");
+    this.load.image("bg5", "images/bg5.png");
+
+
+    this.load.spritesheet('bike', 'images/bike.png', { frameWidth: 140, frameHeight: 200 });
+    this.load.spritesheet('sidewalk', 'images/bg1.png', { frameWidth: 1280, frameHeight: 144 });
+  },
+
+  create: function () {
+    scene = this;
+    this.bg = new BG();
+    // new Radar();
+    this.player = new Player();
+    // this.manhole = new Manhole();
+    
+    // Enemy spawning system
+    this.enemies = [];
+    this.spawnTimer = 0;
+    this.spawnInterval = 120; // Spawn an enemy every 120 frames (~2 seconds at 60 FPS)
+    this.enemySpeed = 3; // Pixels per frame
+  },
+
+  update: function (time) {
+    // Spawn enemies
+    this.bg.update()
+    // this.manhole.update()
+    // this.spawnTimer++;
+    // if (this.spawnTimer >= this.spawnInterval) {
+    //   this.spawnEnemy();
+    //   this.spawnTimer = 0;
+    // }
+    
+    // // Update all enemies
+    // this.enemies.forEach((enemy, index) => {
+    //   if (enemy.active) {
+    //     enemy.update();
+    //   } else {
+    //     // Remove destroyed enemies from the list
+    //     this.enemies.splice(index, 1);
+    //   }
+    // });
+  },
+
+  spawnEnemy: function() {
+    const randomLane = Phaser.Math.Between(1, 3);
+    const enemy = new Enemy(randomLane, this.enemySpeed);
+    this.enemies.push(enemy);
+  }
+});
