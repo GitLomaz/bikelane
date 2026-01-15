5e5e7a0bb504e0da2c095c240bd7a4e658f82892
Ian Lomas <you@example.com>
2026-01-15 14:54:55 -0500

    AUTO-DEPLOYMENT

--- idle/js/classes/radar.js
+++
@@ -1,9 +0,0 @@
-class Radar extends Phaser.GameObjects.Image {
-  constructor() {
-    super(scene, GAME_WIDTH - 140, 0, "radar")
-    this.setOrigin(0)
-    this.setDepth(100)
-    this.setAlpha(.7)
-    scene.add.existing(this)
-  }
-}
