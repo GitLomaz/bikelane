9e04df12f00154b0eb435877890a30affc7a508d
Ian Lomas <you@example.com>
2026-02-22 19:33:47 -0500

    auto-deploy

--- js/classes/scoreItem.js
+++ js/classes/scoreItem.js
@@ -6,29 +6,32 @@ class ScoreItem extends Phaser.GameObjects.Container {
     // let frame = scene.add.image(0, 5, "highscore");
     // frame.setOrigin(0);
     // this.add(frame);
-    let num = scene.add.text(90, 6, displayNumber(number) + ".", {
-      fontFamily: "myFont",
-      fontSize: "24px",
-      stroke: '#330030',
-      strokeThickness: 4
-    });
+    let num = scene.add.bitmapText(90, 6, "normal", displayNumber(number) + ".", 24);
+    // let num = scene.add.text(90, 6, displayNumber(number) + ".", {
+    //   fontFamily: "myFont",
+    //   fontSize: "24px",
+    //   stroke: '#330030',
+    //   strokeThickness: 4
+    // });
     num.setOrigin(1, 0);
     this.add(num);
-    this.add(
-      scene.add.text(98, 6, name, {
-        fontFamily: "myFont",
-        fontSize: "24px",
-        stroke: '#330030',
-        strokeThickness: 4
-      })
-    );
-    this.add(
-      scene.add.text(380, 6, displayNumber(score), {
-        fontFamily: "myFont",
-        fontSize: "24px",
-        stroke: '#330030',
-        strokeThickness: 4
-      })
-    );
+    this.add(scene.add.bitmapText(98, 6, "normal", name, 24))
+    this.add(scene.add.bitmapText(380, 6, "lightNumbers", displayNumber(score), 24))
+    // this.add(
+    //   scene.add.text(98, 6, name, {
+    //     fontFamily: "myFont",
+    //     fontSize: "24px",
+    //     stroke: '#330030',
+    //     strokeThickness: 4
+    //   })
+    // );
+    // this.add(
+    //   scene.add.text(380, 6, displayNumber(score), {
+    //     fontFamily: "myFont",
+    //     fontSize: "24px",
+    //     stroke: '#330030',
+    //     strokeThickness: 4
+    //   })
+    // );
   }
 }
