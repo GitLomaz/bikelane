c6ddaceb2d7122232e839e851f9c62b8a6578c10
Ian Lomas <you@example.com>
2026-05-11 20:51:23 -0400

    bikelane

--- js/scenes/titleScene.js
+++ js/scenes/titleScene.js
@@ -75,7 +75,7 @@ let titleScene = new Phaser.Class({
     this.doodadSpawner = new DoodadSpawner()
     this.doodads = [];
     this.buildingScores = false
-    this.version = this.add.bitmapText(10, 10, "normal", "v1.0.0", 16).setDepth(5)
+    this.version = this.add.bitmapText(10, 10, "normal", "v1.1.0", 16).setDepth(5)
     this.scores = new Button(1140, 675, "highscores", () => {
       if (!this.buildingScores) {
         buildHighScores()
